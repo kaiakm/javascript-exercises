9994cf0c71b2af368f8fb182e1ee71d95c1cbd1c
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-19 21:31:41 -0500

    first test done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    const index = array.indexOf(value);
+    array.splice(index, 1);
+    return array;
+    
 };
 
 // Do not edit below this line
