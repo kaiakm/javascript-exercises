b72d3f32f6666813e8380d21820fa3e97fcd1929
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-21 16:12:55 -0500

    this is so much easier with a cheatsheet

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let fahrenheit = num;
+  let celsius = (fahrenheit - 32) * (5/9);
+
+  if (celsius === 0) {
+    return celsius;
+  } else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let celsius = num;
+  let fahrenheit = ((celsius * (9/5)) + 32);
+
+  if (fahrenheit === 0) {
+    return fahrenheit;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
