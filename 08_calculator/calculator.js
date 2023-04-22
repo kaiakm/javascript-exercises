a1292d4bccb5b9cc33a7aa277414f2959feaec28
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-21 21:52:33 -0500

    next

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sum = 0;
+
+  if (typeof num === "number") {
+    sum = num;
+  } else if (Array.isArray(num)){
+    for (let i = 0; i < num.length ; i++) {
+      sum += num[i];
+    }
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+function multiply(num) {
+  let result = 1;
 
+  for (i = 0; i < num.length; i++) {
+    result *= num[i];
+  }
+
+  return result;
+}
+
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+  let result = 1;
+  
+	if (num === 0) {
+    return 1;
+  } else {
+    for (let i = num; i > 1; i--) {
+      result *= i;
+    }
+  }
 
-const factorial = function() {
-	
+  return result;
 };
 
 // Do not edit below this line
