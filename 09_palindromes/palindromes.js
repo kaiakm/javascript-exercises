5295c313bbc3ee692d951af3f4231c17c84704fa
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-21 22:05:09 -0500

    this whole thing was horrible

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversedStr = str.split('').reverse('').join('');
 
+    return str === reversedStr;
 };
 
 // Do not edit below this line
