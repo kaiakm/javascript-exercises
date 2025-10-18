f158ce967579ea878fbe4a55f1f1668e4443e311
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-10-18 11:22:58 -0500

    completed exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = string.split('').reverse().join('');
+    return word;
 };
 
 // Do not edit below this line
