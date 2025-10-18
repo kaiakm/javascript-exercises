c3bcb6c866f3e17e81a9e41187062ec3be90e016
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-10-18 11:15:14 -0500

    completed exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+function repeatString (string, num) {
+    let word = '';
+    for (let i = 0; i < num; i++) {
+        word += string;
+    };
+    if (num < 0) return 'ERROR';
+    return word;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
